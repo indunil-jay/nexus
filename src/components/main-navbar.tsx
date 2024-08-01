efc4808b3932a3ebc0d4dca0e207bf9065e90d0d
indunil-jay <indunil.jayz@gmail.com>
2024-08-02 05:03:14 +0530

    make header sticky

--- src/components/main-navbar.tsx
+++ src/components/main-navbar.tsx
@@ -4,7 +4,7 @@ import DesktopNav from "@/components/desktop-nav";
 
 const MainNav = () => {
   return (
-    <header className="px-2 pt-2 md:px-10">
+    <header className="px-2 py-2 md:px-10 sticky top-0 bg-white/20 backdrop-blur z-[99999]">
       <div className="flex gap-3 items-center justify-between ">
         <Logo />
         <DesktopNav />
