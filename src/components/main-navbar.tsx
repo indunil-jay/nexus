531d898bf396215588b3b0e18945715b4fd62cb1
indunil-jay <indunil.jayz@gmail.com>
2024-08-02 06:14:06 +0530

    add nav bar transition

--- src/components/main-navbar.tsx
+++ src/components/main-navbar.tsx
@@ -1,15 +1,28 @@
+"use client";
 import Button from "@/components/ui/button";
 import Logo from "@/components/ui/logo";
 import DesktopNav from "@/components/desktop-nav";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import { motionTransition } from "./sections/hero-section";
 
 const MainNav = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
   return (
-    <header className="px-2 py-2 md:px-10 sticky top-0 bg-white/20 backdrop-blur z-[99999]">
-      <div className="flex gap-3 items-center justify-between ">
+    <header
+      ref={ref}
+      className="px-2 py-2 md:px-10 sticky top-0 bg-white/20 backdrop-blur z-[99999]"
+    >
+      <motion.div
+        style={motionTransition(isInView, "translateY(-200px)")}
+        className="flex gap-3 items-center justify-between "
+      >
         <Logo />
         <DesktopNav />
         <Button>Get started</Button>
-      </div>
+      </motion.div>
     </header>
   );
 };
--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -9,11 +9,11 @@ import { useInView } from "framer-motion";
 import { useRef } from "react";
 import { motion } from "framer-motion";
 
-const motionTransition = (isInView: boolean, translation: string) => {
+export const motionTransition = (isInView: boolean, translation: string) => {
   return {
     transform: isInView ? "none" : translation,
     opacity: isInView ? 1 : 0,
-    transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+    transition: `all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s`,
   };
 };
 
